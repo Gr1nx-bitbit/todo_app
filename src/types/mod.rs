17891537b165eb0c82128d7c097d28b12cb4f68a
Gr1nx <gr1nx614@gmail.com>
2025-02-22 00:09:29 -0800

    some base component types for structs

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -9,8 +9,11 @@ struct Lifetime {
     creation_date: DateTime<Local>,
     lifetime_updates: Vec<DateTime<Local>>,
 }
+
+pub type FrequencyScalar = (u32, u32); // frequency in days as well as how much to exponentiate the task priority
+
 #[derive(Debug, Clone)]
 pub enum Multiplier {
-    Scalar(u32),
-    Exponential{ frequency: u32 },
+    Scalar(u32), // how much to multiply a task's priority by
+    Exponential(FrequencyScalar),
 }
