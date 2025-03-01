00351d643edd7260bb866e073063bc2a52064bce
Gr1nx <gr1nx614@gmail.com>
2025-03-01 12:09:01 -0800

    some basic constants that the program can use

---
+++ src/values/mod.rs
@@ -0,0 +1,4 @@
+pub const BASE_PRIORITY_POINTS: u32 = 1;
+pub const BASE_ADDITIVE_POINTS: u32 = 1; // How many points are added to a task's priority count
+pub const BASE_SCALAR: u32 = 2; // Base amount a task's priority count can be multiplied by
+pub const MAX_PRIORITY_COUNT: u32 = 100;
